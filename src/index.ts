67c3bdc1d228e379df4cf4702b1e84ede8c7dbee
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-25 04:53:29 -0300

    feat: add WORDS

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as WORDS } from "./data/words"
 export { default as findProfanity } from "./findProfanity"
 export { default as checkProfanity } from "./checkProfanity"
 export { default as censorProfanity } from "./censorProfanity"
