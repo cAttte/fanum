ce5c2d95157430f0506ebd6535ea7dad53b67d5c
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-28 02:02:22 -0300

    feat(Fanum): implement checkProfanity()

--- src/checkProfanity.ts
+++ src/checkProfanity.ts
@@ -1,1 +1,9 @@
-export default function checkProfanity() {}
+/**
+ * Check whether a string contains profanity.
+ * @param {string} text The text to check profanity for.
+ */
+export default function checkProfanity(text: string): boolean {
+    const matches = this.findProfanity(text)
+    if (matches.length) return true
+    else return false
+}
