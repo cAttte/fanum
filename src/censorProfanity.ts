6bb085b0d80089b78fb291e86afc6e6d946d552b
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-28 02:59:07 -0300

    docs(censorProfanity): update description for CensorOptions.maskIrrelevantCharacters

--- src/censorProfanity.ts
+++ src/censorProfanity.ts
@@ -6,7 +6,7 @@ import replicateTextStyle from "./private/replicateTextStyle"
  * @property {"mask" | "static" | "replace"} mode If `"mask"`, the `mask` option will be repeated to fit every word. If `"static"`, every word will be replaced with the mask, regardless of their length. If `"replace"`, the words will be replaced with their safe counterparts; if they're unavailable, an error will be thrown.
  * @property {string | Array<string>} mask The mask to use with `mode` `"mask"` or `"static"`. If an array, a random element will be chosen for every word.
  * @property {"raw" | "word"} maskLengthBehavior How to detect how long masks should be. If `"raw"`, the input length will be used (`"b a d .w o r d"` will be converted to `"**************"`). If `"word"`, the length of the base word will be used (`"b a d .w o r d"` will be converted to `"*******"`). Used in conjunction with `mode` `"mask"`.
- * @property {boolean} maskIrrelevantCharacters Whether to mask "irrelevant" or non-letter characters. For example, `"f uc  k"` will be masked as `"* **  *"`. Used in conjunction with `maskLengthBehavior` `"raw"` and `mode` `"mask"`.
+ * @property {boolean} maskIrrelevantCharacters Whether to mask "irrelevant" or non-letter characters. For example, if this option is false, `"f uc  k"` will be masked as `"* **  *"`. Used in conjunction with `maskLengthBehavior` `"raw"` and `mode` `"mask"`.
  * @property {boolean} replicateTextStyle Whether to replicate the style of replaced words. For example, `"fUCK"` will be replaced as `"fRICK"`. Used in conjunction with `mode` `"replace"`.
  */
 type CensorOptions = {
