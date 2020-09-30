c4f7c4024a7082d48f897f786507d55451f62d46
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-30 18:25:55 -0300

    feat(findWord, Profanity): implement 'base' property

--- src/private/findWord.ts
+++ src/private/findWord.ts
@@ -4,27 +4,30 @@ import createWordRegex from "./regex/word"
 /**
  * @typedef Profanity
  * @property {number} index The index in the input string of the word.
- * @property {string} word The "base" or "ID" of the word. For example, `"fuck"`.
- * @property {string} raw The actual input as found inside the string. For example, `"Fuc.k"`.
+ * @property {string} word The word found. For example, `"fucks"`.
+ * @property {string} base The "base" word, unmodified, from the word list. For example, `"fuck"`.
+ * @property {string} raw The actual input as found inside the string. For example, `"Fuc.ks"`.
  * @property {string} replacement The replacement of the word, if any. For example, `"frick"`.
  */
 export type Profanity = {
     index: number
     word: string
+    base: string
     raw: string
     replacement: string
 }
 
 export default function findWord(text: string, word: string, base?: string): Profanity[] {
     const profanities = []
-    const regexBody = createWordRegex(base, this.options.maxCharacterSeparation)
+    const regexBody = createWordRegex(word, this.options.maxCharacterSeparation)
     const regex = new RegExp(regexBody, "g")
     let match: RegExpExecArray
 
     while ((match = regex.exec(text)) != null) {
         profanities.push({
             index: match.index,
-            word: base || word,
+            word: word,
+            base: base || word,
             raw: match[0],
             replacement: base
                 ? pluralize(this.options.words[base])
