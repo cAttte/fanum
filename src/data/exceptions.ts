3ec70d00463577a4b8fa587c97f6dd108851ce5f
cAttte <26514199+cAttte@users.noreply.github.com>
2020-09-30 00:51:33 -0300

    refactor(EXCEPTIONS): add a bunch of exceptions

--- src/data/exceptions.ts
+++ src/data/exceptions.ts
@@ -1,20 +1,23 @@
 // TODO: add more exceptions
 
 export default {
+    // WORDS.TAME
+    crap: [
+        "scrap" // scrappy, scrappi-, scrape, skyscraper
+    ],
+    // WORDS.SWEARS
     cock: [
         "cocky",
+        "cockle",
         "bawcock",
         "bibcock",
         "cockade",
-        "cockers",
         "cockeye",
         "cockier",
         "cockies",
         "cockily",
         "cocking",
         "cockish",
-        "cockled",
-        "cockles",
         "cockney",
         "cockpit",
         "cockshy",
@@ -53,7 +56,81 @@ export default {
         "recocked",
         "seacocks",
         "stopcock",
-        "turncock",
-        "woodcock"
+        "turncock"
+    ],
+    dick: [
+        // Dick is a name by itself but what can we do
+        "dickens"
+    ],
+    ass: [
+        "assimilat", // assimilate, assimilation, assimilat-
+        "associat", // associate, association, associative, associat-
+        "assiduous", // assiduous, assiduous-
+        "class", // class, classy, class-, classify, classification, classif-, classic, classic-
+        "embarrass", // embarrass, embarrassing, embarrassment, embarrass-
+        "embarass", // misspelling
+        "assess", // assess, assessment, assessor, assess-
+        "passion", // passion, passionate, passion-, compassion, compassionate, compassion-
+        "cassette", // cassette, videocassette
+        "grass", // grass, grassy
+        "glass", // glass, glassy
+        "pass", // pass, passive, passiv-
+        "sass", // sass, sassy
+        "bass", // bass, bassist, bass-
+        "assault",
+        "embassy",
+        "ambassador",
+        "compass", // compass, encompass, encompass-
+        "assembl", // assemble, assembly, assembler, assembl-
+        "assist", // assist, assistant, assist-
+        "harass", // harass, harassment, harass-
+        "assassin", // assassin, assassinate, assassination, assassin-
+        "mass", // mass, landmass, massage, -mass, mass-, massacre, massacring, massacr-
+        "brass",
+        "assign", // assign, assigned, assignment, assign-
+        "assert", // assert, asserted, assertive, assertion, assert-
+        "assur", // assure, assuring, assuring-, assur-
+        "assort", // assort, assorted, assortment, assort-
+        "thalass" // thalass-
+    ],
+    arse: [
+        "coarse",
+        "hoarse",
+        "sparse",
+        "rehears",
+        "parse",
+        "parsec",
+        "arsen",
+        "catharses"
+    ],
+    cunt: [
+        "scunthorpe" // >:)
+    ],
+    pussy: [
+        // "pussy" can also be used to refer to a cat by itself, but what can we do
+        "pussycat"
+    ],
+    cum: [
+        "scum",
+        "cumin",
+        "circum", // circum-
+        "document", // document, document-
+        "cumulat", // accumulate, accumulative, cumulative, -cumulat-
+        "incumb" // incumb-
+    ],
+    // WORDS.INSULTS
+    twat: [
+        "twater", // saltwater, dirtwater, -twater
+        "twatch", // wristwatch, -twatch
+        "twattle"
+    ],
+    slut: [
+        "slutsk" // (city in Belarus)
+    ],
+    prick: [
+        // "prick" also has non-vulgar meanings but what can we do
+        "pinprick",
+        "prickle",
+        "pricket"
     ]
 }
