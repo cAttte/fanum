9a898864c5e44b1c8f8b41469f47d70f50bce76b
cAttte <26514199+cAttte@users.noreply.github.com>
2020-10-01 23:36:40 -0300

    feat(Fanum): implement extendWordList option

--- src/Fanum.ts
+++ src/Fanum.ts
@@ -27,11 +27,13 @@ type ExceptionList = {
  * @typedef FanumOptions
  * @property {WordList} words The word/replacement list to use.
  * @property {ExceptionList} exceptions The exception list to use.
+ * @property {boolean} extendWordList Whether the provided word and/or exception lists should be "merged" with the default word lists. Useful if you want to extend them and don't want to add the words manually after instantiation. This option is only used within the constructor; mutating it after instantiation has no effect.
  * @property {number} maxCharacterSeparation The maximum number of "irrelevant" characters there can be inbetween the characters of a word. For example, if this option is set to `1`, "f..uck" will no longer be considered profane. Can be Infinity.
  */
 type FanumOptions = {
     words?: WordList
     exceptions?: ExceptionList
+    extendWordList?: boolean
     maxCharacterSeparation?: number
 }
 
@@ -40,21 +42,28 @@ type FanumOptions = {
  * @class
  */
 export default class Fanum {
-    options: FanumOptions = {
-        words: Object.assign(WORDS.SWEARS, WORDS.INSULTS, WORDS.SLURS),
-        exceptions: EXCEPTIONS,
-        maxCharacterSeparation: 25
-    }
+    options: FanumOptions = {}
 
     /**
      * @constructor
      * @param {FanumOptions} options Options to detect profanity.
      */
-    constructor(options: FanumOptions = {}) {
+    constructor({
+        words = Object.assign(WORDS.SWEARS, WORDS.INSULTS, WORDS.SLURS),
+        exceptions = EXCEPTIONS,
+        extendWordList = false,
+        maxCharacterSeparation = 25
+    }: FanumOptions = {}) {
         // [ "word" ] => { word: null }
-        if (Array.isArray(options.words))
-            options.words = Object.fromEntries(options.words.map(w => [w, null]))
-        Object.assign(this.options, options)
+        if (Array.isArray(words)) words = Object.fromEntries(words.map(w => [w, null]))
+        if (extendWordList && typeof words === "object")
+            words = Object.assign(WORDS.SWEARS, WORDS.INSULTS, WORDS.SLURS, words)
+        if (extendWordList && typeof exceptions === "object")
+            exceptions = Object.assign(EXCEPTIONS, exceptions)
+
+        this.options.words = words
+        this.options.exceptions = exceptions
+        this.options.maxCharacterSeparation = maxCharacterSeparation
     }
 
     findProfanity = findProfanity
